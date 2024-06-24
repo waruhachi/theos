7a5959b0c2d3704b4d5106e8f5139997aae0f8c3
roothider <158083651+roothider@users.noreply.github.com>
2024-06-24 19:45:37 +0800

    fix double slash

--- vendor/mod/roothide/instance/framework.mk
+++ vendor/mod/roothide/instance/framework.mk
@@ -1,1 +1,1 @@
-_THEOS_INTERNAL_LDFLAGS += -dynamiclib -install_name "@loader_path/.jbroot/$(LOCAL_INSTALL_PATH)/$(_LOCAL_INSTALL_PATH)"
+_THEOS_INTERNAL_LDFLAGS += -dynamiclib -install_name "@loader_path/.jbroot$(LOCAL_INSTALL_PATH)/$(_LOCAL_INSTALL_PATH)"
